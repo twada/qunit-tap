8139712a123b5cb0fb0a071493694a62f5188446
Takuto Wada <takuto.wada@gmail.com>
2010-06-03 15:02:14 +0900

    narwhal support

--- sample/commonjs/test_helper.js
+++ sample/commonjs/test_helper.js
@@ -1,9 +1,23 @@
 exports = module.exports = global;
 
+var tryRequireThese = function() {
+    var args = Array.prototype.slice.apply(arguments);
+    for(var i=0; i < args.length; i+=1) {
+        try {
+            return require(args[i]);
+        } catch(e) {
+            // ignore
+        }
+    }
+    throw new Error("cannot find moduele: " + args);
+};
+
 QUnit = require("../../vendor/qunit/qunit/qunit").QUnit;
 require("../../lib/qunit-tap");
 
-var sys = require("sys");
+var sys = tryRequireThese("sys", "system");
 for (var i in sys) exports[i] = sys[i];
+
+puts = (typeof sys.puts === 'function') ? sys.puts : sys.print;
 
 exports.assert = QUnit;
