07c006ef88645248246746681fedc526bc5e3cb0
Takuto Wada <takuto.wada@gmail.com>
2012-03-09 20:12:29 +0900

    Automate test_compat.html handling in compatibility spec.

--- test/version_compatibility_spec.rb
+++ test/version_compatibility_spec.rb
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+require 'fileutils'
 
 describe 'QUnit Version Compatibility' do
 
@@ -78,18 +79,34 @@ EOS
   SUITE_DIR = File.expand_path(File.join(File.dirname(__FILE__), 'compatibility'))
   VERSIONS = Dir.glob("#{SUITE_DIR}/*").map{|d| d.split('/').last}.sort.freeze
   QUNIT_RUNNER = File.expand_path(File.join(File.dirname(__FILE__), '..', 'sample', 'js', 'run_qunit.js'))
+  SUITE_FILE_NAME = 'test_compat.html'
+  HEAD_VERSION_TEST_DIR = "#{SUITE_DIR}/current"
+
+  before(:all) do
+    FileUtils::mkdir_p HEAD_VERSION_TEST_DIR
+    FileUtils::cp "#{File.dirname(__FILE__)}/#{SUITE_FILE_NAME}", HEAD_VERSION_TEST_DIR
+    FileUtils::cp "#{File.dirname(__FILE__)}/../vendor/qunit/qunit/qunit.js", HEAD_VERSION_TEST_DIR
+  end
+  after(:all) do
+    FileUtils::rm_rf HEAD_VERSION_TEST_DIR
+  end
 
   VERSIONS.each do |version|
     context "compatibility test upon #{version}" do
       before do
-        @output = `phantomjs #{QUNIT_RUNNER} file://#{SUITE_DIR}/#{version}/test_compat.html`
+        unless File.exist? "#{SUITE_DIR}/#{version}/#{SUITE_FILE_NAME}"
+          FileUtils::cp "#{File.dirname(__FILE__)}/#{SUITE_FILE_NAME}", "#{SUITE_DIR}/#{version}"
+        end
       end
-      subject { @output }
+      after do
+        FileUtils::rm "#{SUITE_DIR}/#{version}/#{SUITE_FILE_NAME}"
+      end
+      let(:output) { `phantomjs #{QUNIT_RUNNER} file://#{SUITE_DIR}/#{version}/#{SUITE_FILE_NAME}` }
       case version
       when "001_two_args"
-        it { should == pretty_old_output }
+        it { output.should == pretty_old_output }
       else
-        it { should == expected_output }
+        it { output.should == expected_output }
       end
     end
   end
