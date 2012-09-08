057fb87b5795c899c452163c7adca1828a0adfcf
Takuto Wada <takuto.wada@gmail.com>
2012-09-08 14:18:45 +0900

    Add empty string message to test edge case.

--- sample/commonjs/test/math_test.js
+++ sample/commonjs/test/math_test.js
@@ -6,7 +6,7 @@ QUnit.module("math module");
 QUnit.test('add' , function() {
                var add = math.add;
                assert.equal(add(1, 4), 5);
-               assert.equal(add(-3, 2), -1);
+               assert.equal(add(-3, 2), -1, '');
                assert.equal(add(1, 3, 4), 8, 'passing 3 args');
                assert.equal(add(2), 2, 'just one arg');
                assert.equal(add(), 0, 'no args');
