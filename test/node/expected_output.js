8589d47bac8e500c03bccc47fa86e9005c9f5397
Takuto Wada <takuto.wada@gmail.com>
2013-01-07 19:05:16 +0900

    Refactor expected_output.js

--- test/node/expected_output.js
+++ test/node/expected_output.js
@@ -1,3 +1,4 @@
+// expected output for specific version
 var argv = require('optimist').argv,
     qunitVersion = argv.version,
     util = require("util"),
@@ -6,23 +7,22 @@ var argv = require('optimist').argv,
     semver = require('semver'),
     before_1_0_0 = function () {
         return !(semver.valid(qunitVersion) || ['stable', 'head'].some(function(v){ return v === qunitVersion; }));
+    },
+    printContent = function (filename) {
+        var content = fs.readFileSync(path.resolve(path.join(__dirname, '..', 'expected', filename)), 'utf8');
+        util.print(content);
     };
 
-// expected output for specific version
-var expectedFile;
 if (qunitVersion === 'stable') {
-    expectedFile = 'latest_format.txt';
+    printContent('latest_format.txt');
 } else if (qunitVersion === 'head') {
-    expectedFile = 'latest_format.txt';
+    printContent('latest_format.txt');
 } else if (qunitVersion === '001_two_args') {
-    expectedFile = '001_format.txt';
+    printContent('001_format.txt');
 } else if (before_1_0_0() || semver.lt(qunitVersion, '1.4.0')) {
-    expectedFile = 'output_before_1_4_0.txt';
+    printContent('output_before_1_4_0.txt');
 } else if (semver.lt(qunitVersion, '1.10.0')) {
-    expectedFile = 'output_before_1_10_0.txt';
+    printContent('output_before_1_10_0.txt');
 } else {
-    expectedFile = 'latest_format.txt';
+    printContent('latest_format.txt');
 }
-
-var content = fs.readFileSync(path.resolve(path.join(__dirname, '..', 'expected', expectedFile)), 'utf8');
-util.print(content);
