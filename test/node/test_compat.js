f70c1370dc117b66be3b53f3ad3dfaf74e684ff5
Takuto Wada <takuto.wada@gmail.com>
2013-08-07 17:07:58 +0900

    Reorganizing regression suite: integrate test_compat.js for Node.js

--- test/node/test_compat.js
+++ test/node/test_compat.js
@@ -1,7 +1,50 @@
-var helper = require("./test_helper").helper,
-    starter = helper.starter;
+var argv = require('optimist').argv,
+    qunitVersion = argv.version,
+    qunitPath = "../compatibility/" + qunitVersion + "/qunit",
+    qunitTap = require("../../lib/qunit-tap"),
+    util = require("util"),
+    semver = require('semver'),
+    slice = Array.prototype.slice,
+    QUnit,
+    tap,
+    before_1_0_0 = function () {
+        return !(semver.valid(qunitVersion) || ['stable', 'head'].some(function(v){ return v === qunitVersion; }));
+    },
+    starter = function () {};
 
-global.QUnit = helper.QUnit;
+// require QUnit (in two ways)
+if (before_1_0_0() || semver.lt(qunitVersion, '1.3.0')) {
+    QUnit = require(qunitPath).QUnit;
+} else {
+    QUnit = require(qunitPath);
+}
+
+tap = qunitTap(QUnit, util.puts, {showSourceOnFailure: false});
+QUnit.init();
+
+if (QUnit.config !== undefined) {
+    QUnit.config.updateRate = 0;
+    if (QUnit.config.semaphore === 1) {
+        QUnit.config.semaphore = 0;
+    }
+}
+
+// starter function (required before 1.3.0)
+if (before_1_0_0() || semver.lt(qunitVersion, '1.3.0')) {
+    starter = function () {
+        QUnit.start();
+    };
+}
+
+var orig = tap.moduleStart;
+tap.moduleStart = function(arg) {
+    orig.apply(tap, slice.apply(arguments));
+    var name = (typeof arg === 'string') ? arg : arg.name;
+    // 'this' refers to tap object
+    this.note('customized: ' + name);
+};
+
+global.QUnit = QUnit;
 global.incr = require("../../sample/commonjs/lib/incr");
 global.math = require("../../sample/commonjs/lib/math");
 
