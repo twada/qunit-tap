77aa769cdc02268b2924a1146a6ee04acfef5512
Takuto Wada <takuto.wada@gmail.com>
2014-03-20 16:35:18 +0900

    Use semver in test_compat_rhino.js

--- test/rhino/test_compat_rhino.js
+++ test/rhino/test_compat_rhino.js
@@ -7,6 +7,11 @@ var qunitVersion = arguments[0],
 loadFile('sample/js/lib/math.js');
 loadFile('sample/js/lib/incr.js');
 
+loadFile('node_modules/semver/semver.js');
+var before_1_0_0 = function () {
+    return !(semver.valid(qunitVersion) || ['stable', 'head'].some(function(v){ return v === qunitVersion; }));
+};
+
 loadFile('test/compatibility/' + qunitVersion + '/qunit.js');
 loadFile('lib/qunit-tap.js');
 
@@ -27,11 +32,23 @@ var tap = qunitTap(QUnit, print, {
     };
 })();
 
-QUnit.init();
-if (QUnit.config !== undefined) {
-    QUnit.config.updateRate = 0;
+
+if (before_1_0_0() || semver.lt(qunitVersion, '1.12.0')) {
+    QUnit.init();
+    if (QUnit.config !== undefined) {
+        QUnit.config.updateRate = 0;
+        if (QUnit.config.semaphore === 1) {
+            QUnit.config.semaphore = 0;
+        }
+    }
+} else if (semver.gte(qunitVersion, '1.13.0')) {
+    QUnit.config.autorun = false;
 }
 
 loadFile('test/suite.js');
 
-QUnit.start();
+if (before_1_0_0() || semver.lt(qunitVersion, '1.11.0')) {
+    QUnit.start();
+} else if (semver.gte(qunitVersion, '1.13.0')) {
+    QUnit.load();
+};
